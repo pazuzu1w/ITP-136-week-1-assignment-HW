7ef1d4b34064984de412782f8196334f684109b5
Anthony Andrews <aaa76743@email.vccs.edu>
2024-01-28 22:22:32 -0500

    Add project files.

---
+++ ITP 136 week 1 assignment HW/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ITP_136_week_1_assignment_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patientName;
+
+            Console.WriteLine("Who is being seen today?");
+
+            patientName = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + patientName);
+
+            Console.ReadKey();
+        }
+    }
+}
